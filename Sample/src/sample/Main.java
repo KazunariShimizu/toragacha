303ba85616882a1c21abcda33636ba9b82ce8168
BuildTools <unconfigured@null.spigotmc.org>
2018-12-13 12:37:40 +0900

    新規

---
+++ Sample/src/sample/Main.java
@@ -0,0 +1,14 @@
+package sample;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		System.out.println("プラグインが有効になりました");
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("プラグインが無効になりました");
+	}
+}
